12a55817028caa21c858d854e4761f051039f4c4
BhagyodayIngale <ingale.bhagyoday@gmail.com>
2020-01-14 19:22:44 +0530

    Assignment To sort Letters  and get count in given string and remove special characters.

---
+++ BhagyoLetterCount/BhagyoLetterCount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BhagyoLetterCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Regex MyRegex = new Regex("[^a-z]", RegexOptions.IgnoreCase);
+            string name = "B,ha'@gy$.od?a>y";
+            string s = MyRegex.Replace(name, @"");
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            dict = p.getcount(s);
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key.ToString() + "   " + item.Value.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        public Dictionary<char, int> getcount(string name)
+        {
+            return name.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+        }
+    }
+}
