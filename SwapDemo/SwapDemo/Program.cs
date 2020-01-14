01e63e0c342f8150dce23668224470d1e9df9ae3
swappwr96 <swappwr96@gmail.com>
2020-01-14 20:00:01 +0530

    I was create my branch  and add demo project....

---
+++ SwapDemo/SwapDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Namaste all");
+            Console.ReadLine();
+        }
+    }
+}
